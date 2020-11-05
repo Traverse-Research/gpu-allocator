67b1a12a07db328e6a20f5eb5ab1497af1f3069f
Jasper Bekkers <bekkers@gmail.com>
2020-11-05 17:47:19 +0100

    Remove old `mod`

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,6 @@ use ash::vk;
 use log::{log, Level};
 use std::cell::RefCell;
 
-pub mod memory_location;
-pub use memory_location::*;
-
 mod result;
 pub use result::*;
 
@@ -415,6 +412,30 @@ impl MemoryType {
         Ok(())
     }
 }
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum MemoryLocation {
+    Unknown,
+    GpuOnly,
+    CpuToGpu,
+    GpuToCpu,
+}
+
+fn find_memorytype_index(
+    memory_req: &vk::MemoryRequirements,
+    memory_prop: &vk::PhysicalDeviceMemoryProperties,
+    flags: vk::MemoryPropertyFlags,
+) -> Option<u32> {
+    memory_prop.memory_types[..memory_prop.memory_type_count as _]
+        .iter()
+        .enumerate()
+        .find(|(index, memory_type)| {
+            (1 << index) & memory_req.memory_type_bits != 0
+                && memory_type.property_flags & flags == flags
+        })
+        .map(|(index, _memory_type)| index as _)
+}
+
 
 struct GpuAllocator {
     memory_types: Vec<MemoryType>,
