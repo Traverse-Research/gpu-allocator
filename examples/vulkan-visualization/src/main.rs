Traverse-Research/gpu-allocator

commit 2e1f275abf2977efd8ee116ee3ffe61fcb5153f2

Update ash to 0.32 (#17)
After months of hard work, additions and fixes, ash finally broke a new
release :partying_face:!

Cargo.toml
@@ -17,7 +17,7 @@ include = [
 ]
 
 [dependencies]
-ash = "0.31"
+ash = "0.32"
 backtrace = "0.3"
 log = "0.4"
 thiserror = "1.0"

examples/vulkan-buffer/src/main.rs
@@ -9,7 +9,7 @@ use gpu_allocator::{
 };
 
 fn main() {
-    let entry = ash::Entry::new().unwrap();
+    let entry = unsafe { ash::Entry::new() }.unwrap();
 
     // Create vulkan instance
     let instance = {

commit dee796c103be2138b5cf8d7afdc9a1cd9e8948ad

Update example to ash 0.32 and ash-window to 0.6
These were completely forgotten about in the CI.

Fixes: 2e1f275 ("Update ash to 0.32 (#17)")

Cargo.toml
@@ -24,7 +24,7 @@ thiserror = "1.0"
 imgui = { version = "0.6", optional = true }
 
 [dev-dependencies]
-ash-window = "0.5"
+ash-window = "0.6"
 winit = "0.23"
 
 

examples/vulkan-visualization/src/main.rs
@@ -13,7 +13,7 @@ mod imgui_renderer;
 use imgui_renderer::{handle_imgui_event, ImGuiRenderer};
 
 fn main() {
-    let entry = ash::Entry::new().unwrap();
+    let entry = unsafe { ash::Entry::new() }.unwrap();
 
     let event_loop = winit::event_loop::EventLoop::new();
 