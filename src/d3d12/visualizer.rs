4b91b09718fee0a0a207228ca9bf012388335949
Jasper Bekkers <bekkers@gmail.com>
2022-09-02 14:44:25 +0200

    Add tables-api feature

--- src/d3d12/visualizer.rs
+++ src/d3d12/visualizer.rs
@@ -289,9 +289,6 @@ impl AllocatorVisualizer {
     /// - If [`true`], the widget will be drawn and an (X) closing button will be added to the widget bar.
     pub fn render(&mut self, allocator: &Allocator, ui: &imgui::Ui<'_>, opened: Option<&mut bool>) {
         if opened != Some(&mut false) {
-            // self.render_main_window(ui, opened, allocator);
-            // self.render_memory_block_windows(ui, allocator);
-
             self.render_breakdown(allocator, ui, opened);
         }
     }
