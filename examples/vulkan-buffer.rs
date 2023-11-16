407360bf04a5d8c2bd5e3617b415e4df3cbc27b3
Marijn Suijten <marijn@traverseresearch.nl>
2023-11-16 11:58:40 +0100

    Clean up Vulkan example

--- examples/vulkan-buffer.rs
+++ examples/vulkan-buffer.rs
@@ -1,8 +1,8 @@
+use std::default::Default;
+use std::ffi::CStr;
+
 use ash::vk;
 use log::info;
-
-use std::default::Default;
-use std::ffi::CString;
 
 use gpu_allocator::vulkan::{
     AllocationCreateDesc, AllocationScheme, Allocator, AllocatorCreateDesc,
@@ -16,27 +16,22 @@ fn main() {
 
     // Create Vulkan instance
     let instance = {
-        let app_name = CString::new("Vulkan gpu-allocator test").unwrap();
+        let app_name = CStr::from_bytes_with_nul(b"Vulkan gpu-allocator test\0").unwrap();
 
         let appinfo = vk::ApplicationInfo::builder()
-            .application_name(&app_name)
+            .application_name(app_name)
             .application_version(0)
-            .engine_name(&app_name)
+            .engine_name(app_name)
             .engine_version(0)
             .api_version(vk::make_api_version(0, 1, 0, 0));
 
-        let layer_names = [CString::new("VK_LAYER_KHRONOS_validation").unwrap()];
-        let layers_names_raw: Vec<*const i8> = layer_names
-            .iter()
-            .map(|raw_name| raw_name.as_ptr())
-            .collect();
-
-        let extensions_names_raw = vec![];
+        let layer_names_raw = [CStr::from_bytes_with_nul(b"VK_LAYER_KHRONOS_validation\0")
+            .unwrap()
+            .as_ptr()];
 
         let create_info = vk::InstanceCreateInfo::builder()
             .application_info(&appinfo)
-            .enabled_layer_names(&layers_names_raw)
-            .enabled_extension_names(&extensions_names_raw);
+            .enabled_layer_names(&layer_names_raw);
 
         unsafe {
             entry
