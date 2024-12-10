e07cabd335b3a2f9a9c0a98deefa49e378d922f7
Marijn Suijten <marijn@traverseresearch.nl>
2024-12-10 14:53:27 +0100

    Remove stale and UB `winapi` compatibility layer
    With the `windows` migration we introduced a `winapi` compatibility
    layer to make it easier to use `winapi` types with `gpu-allocator`.  As
    most of the ecosystem has switched to `windows` it is now time to sunset
    these helpers which are otherwise tedious to test and maintain, not to
    mention contain long(er) standing unsoundness bugs.

--- examples/d3d12-buffer.rs
+++ examples/d3d12-buffer.rs
@@ -1,59 +1,53 @@
-//! Example showcasing [`winapi`] interop with [`gpu-allocator`] which is driven by the [`windows`] crate.
-use winapi::{
-    shared::{dxgiformat, winerror},
-    um::{d3d12, d3dcommon},
-    Interface,
-};
-
-mod all_dxgi {
-    pub use winapi::shared::{dxgi1_3::*, dxgi1_6::*, dxgitype::*};
-}
-
+//! Example showcasing [`gpu-allocator`] with types and functions from the [`windows`] crate.
 use gpu_allocator::{
     d3d12::{
-        AllocationCreateDesc, Allocator, AllocatorCreateDesc, ID3D12DeviceVersion,
-        ResourceCategory, ToWinapi, ToWindows,
+        AllocationCreateDesc, Allocator, AllocatorCreateDesc, ID3D12DeviceVersion, ResourceCategory,
     },
     MemoryLocation,
 };
 use log::*;
+use windows::{
+    core::{Interface, Result},
+    Win32::{
+        Foundation::E_NOINTERFACE,
+        Graphics::{
+            Direct3D::{D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_12_0},
+            Direct3D12::{
+                D3D12CreateDevice, ID3D12Device, ID3D12Resource,
+                D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT, D3D12_RESOURCE_DESC,
+                D3D12_RESOURCE_DIMENSION_BUFFER, D3D12_RESOURCE_FLAG_NONE,
+                D3D12_RESOURCE_STATE_COMMON, D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+            },
+            Dxgi::{
+                Common::{DXGI_FORMAT_UNKNOWN, DXGI_SAMPLE_DESC},
+                CreateDXGIFactory2, IDXGIAdapter4, IDXGIFactory6, DXGI_ADAPTER_FLAG3_SOFTWARE,
+                DXGI_ERROR_NOT_FOUND,
+            },
+        },
+    },
+};
 
-fn create_d3d12_device(
-    dxgi_factory: *mut all_dxgi::IDXGIFactory6,
-) -> Option<*mut d3d12::ID3D12Device> {
+fn create_d3d12_device(dxgi_factory: &IDXGIFactory6) -> Option<ID3D12Device> {
     for idx in 0.. {
-        let mut adapter4: *mut all_dxgi::IDXGIAdapter4 = std::ptr::null_mut();
-        let hr = unsafe {
-            dxgi_factory.as_ref().unwrap().EnumAdapters1(
-                idx,
-                <*mut *mut all_dxgi::IDXGIAdapter4>::cast(&mut adapter4),
-            )
+        // TODO: Might as well return Result<> from this function
+        let adapter1 = match unsafe { dxgi_factory.EnumAdapters1(idx) } {
+            Ok(a) => a,
+            Err(e) if e.code() == DXGI_ERROR_NOT_FOUND => break,
+            Err(e) => panic!("{:?}", e),
         };
+        let adapter4: IDXGIAdapter4 = adapter1.cast().unwrap();
 
-        if hr == winerror::DXGI_ERROR_NOT_FOUND {
-            break;
-        }
-
-        assert_eq!(hr, winerror::S_OK);
-
-        let mut desc = all_dxgi::DXGI_ADAPTER_DESC3::default();
-        let hr = unsafe { adapter4.as_ref().unwrap().GetDesc3(&mut desc) };
-        if hr != winerror::S_OK {
-            error!("Failed to get adapter description for adapter");
-            continue;
-        }
-
+        let desc = unsafe { adapter4.GetDesc3() }.unwrap();
         // Skip software adapters
-        if (desc.Flags & all_dxgi::DXGI_ADAPTER_FLAG3_SOFTWARE)
-            == all_dxgi::DXGI_ADAPTER_FLAG3_SOFTWARE
-        {
+        // Vote for https://github.com/microsoft/windows-rs/issues/793!
+        if (desc.Flags & DXGI_ADAPTER_FLAG3_SOFTWARE) == DXGI_ADAPTER_FLAG3_SOFTWARE {
             continue;
         }
 
         let feature_levels = [
-            (d3dcommon::D3D_FEATURE_LEVEL_11_0, "D3D_FEATURE_LEVEL_11_0"),
-            (d3dcommon::D3D_FEATURE_LEVEL_11_1, "D3D_FEATURE_LEVEL_11_1"),
-            (d3dcommon::D3D_FEATURE_LEVEL_12_0, "D3D_FEATURE_LEVEL_12_0"),
+            (D3D_FEATURE_LEVEL_11_0, "D3D_FEATURE_LEVEL_11_0"),
+            (D3D_FEATURE_LEVEL_11_1, "D3D_FEATURE_LEVEL_11_1"),
+            (D3D_FEATURE_LEVEL_12_0, "D3D_FEATURE_LEVEL_12_0"),
         ];
 
         let device =
@@ -61,28 +55,20 @@ fn create_d3d12_device(
                 .iter()
                 .rev()
                 .find_map(|&(feature_level, feature_level_name)| {
-                    let mut device: *mut d3d12::ID3D12Device = std::ptr::null_mut();
-                    let hr = unsafe {
-                        d3d12::D3D12CreateDevice(
-                            adapter4.cast(),
-                            feature_level,
-                            &d3d12::ID3D12Device::uuidof(),
-                            <*mut *mut d3d12::ID3D12Device>::cast(&mut device),
-                        )
-                    };
-                    match hr {
-                        winerror::S_OK => {
-                            info!("Using D3D12 feature level: {}.", feature_level_name);
-                            Some(device)
+                    let mut device = None;
+                    match unsafe { D3D12CreateDevice(&adapter4, feature_level, &mut device) } {
+                        Ok(()) => {
+                            info!("Using D3D12 feature level: {}", feature_level_name);
+                            Some(device.unwrap())
                         }
-                        winerror::E_NOINTERFACE => {
-                            error!("ID3D12Device interface not supported.");
+                        Err(e) if e.code() == E_NOINTERFACE => {
+                            error!("ID3D12Device interface not supported");
                             None
                         }
-                        _ => {
+                        Err(e) => {
                             info!(
-                                "D3D12 feature level: {} not supported: {:x}",
-                                feature_level_name, hr
+                                "D3D12 feature level {} not supported: {}",
+                                feature_level_name, e
                             );
                             None
                         }
@@ -96,78 +82,62 @@ fn create_d3d12_device(
     None
 }
 
-fn main() {
+fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace")).init();
 
-    let dxgi_factory = {
-        let mut dxgi_factory: *mut all_dxgi::IDXGIFactory6 = std::ptr::null_mut();
-        let hr = unsafe {
-            all_dxgi::CreateDXGIFactory2(
-                0,
-                &all_dxgi::IID_IDXGIFactory6,
-                <*mut *mut all_dxgi::IDXGIFactory6>::cast(&mut dxgi_factory),
-            )
-        };
+    let dxgi_factory = unsafe {
+        CreateDXGIFactory2(windows::Win32::Graphics::Dxgi::DXGI_CREATE_FACTORY_FLAGS::default())
+    }?;
 
-        assert_eq!(hr, winerror::S_OK, "Failed to create DXGI factory");
-        dxgi_factory
-    };
-
-    let device = create_d3d12_device(dxgi_factory).expect("Failed to create D3D12 device.");
+    let device = create_d3d12_device(&dxgi_factory).expect("Failed to create D3D12 device.");
 
     // Setting up the allocator
     let mut allocator = Allocator::new(&AllocatorCreateDesc {
-        device: ID3D12DeviceVersion::Device(device.as_windows().clone()),
+        device: ID3D12DeviceVersion::Device(device.clone()),
         debug_settings: Default::default(),
         allocation_sizes: Default::default(),
     })
     .unwrap();
 
-    let device = unsafe { device.as_ref() }.unwrap();
-
     // Test allocating Gpu Only memory
     {
-        let test_buffer_desc = d3d12::D3D12_RESOURCE_DESC {
-            Dimension: d3d12::D3D12_RESOURCE_DIMENSION_BUFFER,
-            Alignment: d3d12::D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT as u64,
+        let test_buffer_desc = D3D12_RESOURCE_DESC {
+            Dimension: D3D12_RESOURCE_DIMENSION_BUFFER,
+            Alignment: D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT as u64,
             Width: 512,
             Height: 1,
             DepthOrArraySize: 1,
             MipLevels: 1,
-            Format: dxgiformat::DXGI_FORMAT_UNKNOWN,
-            SampleDesc: all_dxgi::DXGI_SAMPLE_DESC {
+            Format: DXGI_FORMAT_UNKNOWN,
+            SampleDesc: DXGI_SAMPLE_DESC {
                 Count: 1,
                 Quality: 0,
             },
-            Layout: d3d12::D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
-            Flags: d3d12::D3D12_RESOURCE_FLAG_NONE,
+            Layout: D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+            Flags: D3D12_RESOURCE_FLAG_NONE,
         };
 
-        let allocation_desc = AllocationCreateDesc::from_winapi_d3d12_resource_desc(
-            device,
+        let allocation_desc = AllocationCreateDesc::from_d3d12_resource_desc(
+            allocator.device(),
             &test_buffer_desc,
-            "Test allocation (Gpu Only)",
+            "Test allocation (Gpu only)",
             MemoryLocation::GpuOnly,
         );
         let allocation = allocator.allocate(&allocation_desc).unwrap();
 
-        let mut resource: *mut d3d12::ID3D12Resource = std::ptr::null_mut();
-        let hr = unsafe {
+        let mut resource: Option<ID3D12Resource> = None;
+        unsafe {
             device.CreatePlacedResource(
-                allocation.heap().as_winapi() as *mut _,
+                allocation.heap(),
                 allocation.offset(),
                 &test_buffer_desc,
-                d3d12::D3D12_RESOURCE_STATE_COMMON,
-                std::ptr::null(),
-                &d3d12::IID_ID3D12Resource,
-                <*mut *mut d3d12::ID3D12Resource>::cast(&mut resource),
+                D3D12_RESOURCE_STATE_COMMON,
+                None,
+                &mut resource,
             )
-        };
-        if hr != winerror::S_OK {
-            panic!("Failed to create placed resource.");
-        }
+        }?;
 
-        unsafe { resource.as_ref().unwrap().Release() };
+        drop(resource);
 
         allocator.free(allocation).unwrap();
         info!("Allocation and deallocation of GpuOnly memory was successful.");
@@ -175,27 +145,27 @@ fn main() {
 
     // Test allocating Cpu to Gpu memory
     {
-        let test_buffer_desc = d3d12::D3D12_RESOURCE_DESC {
-            Dimension: d3d12::D3D12_RESOURCE_DIMENSION_BUFFER,
-            Alignment: d3d12::D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT as u64,
+        let test_buffer_desc = D3D12_RESOURCE_DESC {
+            Dimension: D3D12_RESOURCE_DIMENSION_BUFFER,
+            Alignment: D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT as u64,
             Width: 512,
             Height: 1,
             DepthOrArraySize: 1,
             MipLevels: 1,
-            Format: dxgiformat::DXGI_FORMAT_UNKNOWN,
-            SampleDesc: all_dxgi::DXGI_SAMPLE_DESC {
+            Format: DXGI_FORMAT_UNKNOWN,
+            SampleDesc: DXGI_SAMPLE_DESC {
                 Count: 1,
                 Quality: 0,
             },
-            Layout: d3d12::D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
-            Flags: d3d12::D3D12_RESOURCE_FLAG_NONE,
+            Layout: D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+            Flags: D3D12_RESOURCE_FLAG_NONE,
         };
 
-        let alloc_info = unsafe { device.GetResourceAllocationInfo(0, 1, &test_buffer_desc) };
+        let alloc_info = unsafe { device.GetResourceAllocationInfo(0, &[test_buffer_desc]) };
 
         let allocation = allocator
             .allocate(&AllocationCreateDesc {
-                name: "Test allocation (Cpu to Gpu)",
+                name: "Test allocation (Cpu To Gpu)",
                 location: MemoryLocation::CpuToGpu,
                 size: alloc_info.SizeInBytes,
                 alignment: alloc_info.Alignment,
@@ -203,23 +173,19 @@ fn main() {
             })
             .unwrap();
 
-        let mut resource: *mut d3d12::ID3D12Resource = std::ptr::null_mut();
-        let hr = unsafe {
+        let mut resource: Option<ID3D12Resource> = None;
+        unsafe {
             device.CreatePlacedResource(
-                allocation.heap().as_winapi() as *mut _,
+                allocation.heap(),
                 allocation.offset(),
                 &test_buffer_desc,
-                d3d12::D3D12_RESOURCE_STATE_COMMON,
-                std::ptr::null(),
-                &d3d12::IID_ID3D12Resource,
-                <*mut *mut d3d12::ID3D12Resource>::cast(&mut resource),
+                D3D12_RESOURCE_STATE_COMMON,
+                None,
+                &mut resource,
             )
-        };
-        if hr != winerror::S_OK {
-            panic!("Failed to create placed resource.");
-        }
+        }?;
 
-        unsafe { resource.as_ref().unwrap().Release() };
+        drop(resource);
 
         allocator.free(allocation).unwrap();
         info!("Allocation and deallocation of CpuToGpu memory was successful.");
@@ -227,23 +193,23 @@ fn main() {
 
     // Test allocating Gpu to Cpu memory
     {
-        let test_buffer_desc = d3d12::D3D12_RESOURCE_DESC {
-            Dimension: d3d12::D3D12_RESOURCE_DIMENSION_BUFFER,
-            Alignment: d3d12::D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT as u64,
+        let test_buffer_desc = D3D12_RESOURCE_DESC {
+            Dimension: D3D12_RESOURCE_DIMENSION_BUFFER,
+            Alignment: D3D12_DEFAULT_RESOURCE_PLACEMENT_ALIGNMENT as u64,
             Width: 512,
             Height: 1,
             DepthOrArraySize: 1,
             MipLevels: 1,
-            Format: dxgiformat::DXGI_FORMAT_UNKNOWN,
-            SampleDesc: all_dxgi::DXGI_SAMPLE_DESC {
+            Format: DXGI_FORMAT_UNKNOWN,
+            SampleDesc: DXGI_SAMPLE_DESC {
                 Count: 1,
                 Quality: 0,
             },
-            Layout: d3d12::D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
-            Flags: d3d12::D3D12_RESOURCE_FLAG_NONE,
+            Layout: D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+            Flags: D3D12_RESOURCE_FLAG_NONE,
         };
 
-        let alloc_info = unsafe { device.GetResourceAllocationInfo(0, 1, &test_buffer_desc) };
+        let alloc_info = unsafe { device.GetResourceAllocationInfo(0, &[test_buffer_desc]) };
 
         let allocation = allocator
             .allocate(&AllocationCreateDesc {
@@ -255,29 +221,23 @@ fn main() {
             })
             .unwrap();
 
-        let mut resource: *mut d3d12::ID3D12Resource = std::ptr::null_mut();
-        let hr = unsafe {
+        let mut resource: Option<ID3D12Resource> = None;
+        unsafe {
             device.CreatePlacedResource(
-                allocation.heap().as_winapi() as *mut _,
+                allocation.heap(),
                 allocation.offset(),
                 &test_buffer_desc,
-                d3d12::D3D12_RESOURCE_STATE_COMMON,
-                std::ptr::null(),
-                &d3d12::IID_ID3D12Resource,
-                <*mut *mut d3d12::ID3D12Resource>::cast(&mut resource),
+                D3D12_RESOURCE_STATE_COMMON,
+                None,
+                &mut resource,
             )
-        };
-        if hr != winerror::S_OK {
-            panic!("Failed to create placed resource.");
-        }
+        }?;
 
-        unsafe { resource.as_ref().unwrap().Release() };
+        drop(resource);
 
         allocator.free(allocation).unwrap();
         info!("Allocation and deallocation of CpuToGpu memory was successful.");
     }
 
-    drop(allocator); // Explicitly drop before destruction of device.
-    unsafe { device.Release() };
-    unsafe { dxgi_factory.as_ref().unwrap().Release() };
+    Ok(())
 }
